764c4bf301f0ef81f73be85b8e1c383b20611417
zzztttkkk <ztkisalreadytaken@gmail.com>
2023-12-14 18:24:26 +0800

    [🧱chore] (/): -

--- echo.primary/logging/RotationAppender.cs
+++ echo.primary/logging/RotationAppender.cs
@@ -1,40 +1,218 @@
-﻿namespace echo.primary.logging;
+﻿using System.Text;
+using System.Threading.Channels;
+using echo.primary.utils;
 
-using Ini = utils.Ini;
+namespace echo.primary.logging;
 
 public class RotationOptions {
 	[Ini(Required = true)] public string FileName { get; set; } = "";
-	[Ini(Required = true)] public ulong BySize { get; set; } = 0;
+	[Ini(Required = true)] public long BySize { get; set; } = 0;
 	[Ini(Required = true)] public bool ByDaily { get; set; } = false;
+	[Ini] public int BufferSize { get; set; } = 4096;
 }
 
-public class RotationAppender(RotationOptions opts) : IAppender {
-	private RotationOptions _options = opts;
+public class RotationAppender : IAppender {
+	private readonly RotationOptions _options;
 	public Level Level { get; set; } = Level.TRACE;
 	public string Name { get; set; } = "";
 	public IRenderer Renderer { get; set; } = new SimpleLineRenderer();
 
-	private SemaphoreSlim _streamSlim = new(1, 1);
-	private FileStream? _stream = null;
+	private FileStream? _stream;
+	private long _fileSize;
+	private DirectoryInfo? _directoryInfo;
+	private readonly string filebasename;
+	private readonly string fileextname;
 
-	private SemaphoreSlim _tmpSlim = new(1, 1);
-	private LinkedList<LogItem> _tmp = new();
+	private bool _stopping;
+	private readonly TaskCompletionSource _writeDoneTcs = new();
+	private CancellationTokenSource? _readCts;
 
-	private async Task AppendAsync(LogItem log) {
-		await _tmpSlim.WaitAsync();
-		try {
-			_tmp.AddLast(log);
+	private readonly Channel<LogItem> _channel = Channel.CreateUnbounded<LogItem>();
+
+	private readonly StringBuilder tmp;
+
+	public RotationAppender(RotationOptions opts) {
+		_options = opts;
+		if (_options is { ByDaily: false, BySize: < 1 } or { ByDaily: true, BySize: > 1 }) {
+			throw new Exception("ByDaily or BySize all true or all false");
 		}
-		finally {
-			_tmpSlim.Release();
+
+		if (string.IsNullOrEmpty(_options.FileName)) {
+			throw new Exception("empty FileName");
 		}
+
+		var idx = _options.FileName.LastIndexOf('.');
+		if (idx < 0) {
+			filebasename = _options.FileName;
+			fileextname = "";
+		}
+		else {
+			filebasename = _options.FileName[..idx].Trim();
+			fileextname = _options.FileName[(idx + 1)..].Trim();
+		}
+
+		if (_options.BufferSize < 4096) _options.BufferSize = 4096;
+		tmp = new StringBuilder(_options.BufferSize);
+		_ = WriteLoopTask();
+	}
+
+	private Task Write(string line) {
+		tmp.Append(line);
+		return tmp.Length < _options.BufferSize ? Task.CompletedTask : WriteTmpToFile();
+	}
+
+	private void RenameBySize() {
+		long idx = 0;
+		if (_directoryInfo == null) {
+			idx = (long)Time.unixmills();
+		}
+		else {
+			foreach (var name in _directoryInfo.GetFiles().Select(v => v.Name).Where(v => v.StartsWith(filebasename))) {
+				if (fileextname.Length > 0 && !name.EndsWith(fileextname)) {
+					continue;
+				}
+
+				try {
+					var txt = name[filebasename.Length..^fileextname.Length];
+					var num = Convert.ToInt64(txt);
+					if (num > idx) idx = num;
+				}
+				catch {
+					// ignored
+				}
+			}
+
+			idx++;
+		}
+
+		var nfn = $"{filebasename}{idx}";
+		if (fileextname.Length > 0) {
+			nfn += $".{fileextname}";
+		}
+
+		DoRename(nfn);
+	}
+
+	private void RenameByDay(DateTime time) {
+		var nbasefn = $"{filebasename}_{time.Year}_{time.Month}_{time.Day}";
+		var nfn = nbasefn;
+		if (fileextname.Length > 0) {
+			nfn += $"{nfn}.{fileextname}";
+		}
+
+		if (File.Exists(nfn)) {
+			nfn = $"{nbasefn}.{Time.unixmills}";
+			if (fileextname.Length > 0) {
+				nfn += $"{nfn}.{fileextname}";
+			}
+		}
+
+		DoRename(nfn);
+	}
+
+	private void DoRename(string name) {
+		_stream!.Flush();
+		_stream = null;
+		_directoryInfo = null;
+		_fileSize = 0;
+		File.Move(_options.FileName, name);
+	}
+
+	private async Task<int> WriteTmpToFileReal() {
+		var buf = Encoding.UTF8.GetBytes(tmp.ToString());
+		await _stream!.WriteAsync(buf);
+		tmp.Clear();
+		return buf.Length;
+	}
+
+	private async Task WriteTmpToFile() {
+		while (true) {
+			if (_stream == null) {
+				var info = new FileInfo(_options.FileName);
+				_directoryInfo = info.Directory;
+				if (info.Exists) {
+					if (_options.ByDaily) {
+						var now = DateTime.Now;
+						var begin = Time.unix(new DateTime(now.Year, now.Month, now.Day));
+						var end = begin + 86399;
+						var fct = Time.unix(info.CreationTime);
+						if (fct < begin || fct > end) {
+							RenameByDay(info.CreationTime);
+							continue;
+						}
+					}
+
+					_stream = info.OpenWrite();
+					_fileSize = info.Length;
+				}
+				else {
+					_stream = info.Create();
+					_fileSize = 0;
+				}
+			}
+
+			var size = await WriteTmpToFileReal();
+			if (_options.BySize > 0) {
+				_fileSize += size;
+				if (_fileSize > _options.BySize) {
+					RenameBySize();
+				}
+			}
+
+			break;
+		}
+	}
+
+	private async Task WriteLoopTask() {
+		DateTime? prevTime = null;
+		while (!_stopping) {
+			_readCts = new CancellationTokenSource();
+			try {
+				var log = await _channel.Reader.ReadAsync(_readCts.Token);
+				if (_options.ByDaily) {
+					if (prevTime != null && log.time.Day != prevTime.Value.Day) {
+						if (tmp.Length > 0) {
+							await WriteTmpToFile();
+						}
+
+						RenameByDay(prevTime.Value);
+					}
+
+					prevTime = log.time;
+				}
+
+				await Write(Renderer.Render(Name, log));
+			}
+			catch (TaskCanceledException) {
+				break;
+			}
+		}
+
+		while (true) {
+			_channel.Reader.TryRead(out var log);
+			if (log == null) break;
+			await Write(Renderer.Render(Name, log));
+		}
+
+		if (tmp.Length > 0) {
+			await WriteTmpToFile();
+		}
+
+		if (_stream != null) {
+			await _stream.FlushAsync();
+		}
+
+		_writeDoneTcs.SetResult();
 	}
 
 	public void Append(LogItem log) {
-		_ = AppendAsync(log);
+		if (_stopping) return;
+		_channel.Writer.WriteAsync(log).AsTask();
 	}
 
 	public void Flush() {
-		throw new NotImplementedException();
+		_stopping = true;
+		_readCts?.Cancel();
+		_writeDoneTcs.Task.Wait();
 	}
 }
--- echo.primary/utils/Time.cs
+++ echo.primary/utils/Time.cs
@@ -1,9 +1,14 @@
 ﻿namespace echo.primary.utils;
 
 public static class Time {
-	private static readonly DateTime zero = new(1970, 1, 1);
+	private static readonly DateTime zero = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-	public static ulong unix => (ulong)DateTime.UtcNow.Subtract(zero).TotalSeconds;
-	public static ulong unixmills => (ulong)DateTime.UtcNow.Subtract(zero).TotalMilliseconds;
-	public static ulong unixnanos => (ulong)DateTime.UtcNow.Subtract(zero).TotalNanoseconds;
+	public static ulong unixnanos(DateTime? v = null) =>
+		(ulong)(v?.ToUniversalTime() ?? DateTime.UtcNow).Subtract(zero).TotalNanoseconds;
+
+	public static ulong unixmills(DateTime? v = null) =>
+		(ulong)(v?.ToUniversalTime() ?? DateTime.UtcNow).Subtract(zero).TotalMilliseconds;
+
+	public static ulong unix(DateTime? v = null) =>
+		(ulong)(v?.ToUniversalTime() ?? DateTime.UtcNow).Subtract(zero).TotalSeconds;
 }
