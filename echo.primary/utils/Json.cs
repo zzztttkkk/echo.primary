007b12d0567de71ba41f492bdb55c4fa71fa1c0a
zzztttkkk <ztkisalreadytaken@gmail.com>
2023-12-19 21:48:08 +0800

    [🧱chore] (/): -

---
+++ echo.primary/utils/Json.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace echo.primary.utils;
+
+class ColorConverter : JsonConverter<Color> {
+	public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+		throw new NotImplementedException();
+	}
+
+	public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options) {
+		writer.WriteStringValue(value.Name);
+	}
+}
+
+public static class JSON {
+	private static readonly List<JsonConverter> Converters = new() {
+		new ColorConverter()
+	};
+
+	public static void AddCustomConverter(JsonConverter converter) {
+		Converters.Add(converter);
+	}
+
+	private static JsonSerializerOptions opts {
+		get {
+			var _opts = new JsonSerializerOptions();
+			foreach (var converter in Converters) {
+				_opts.Converters.Add(converter);
+			}
+
+			return _opts;
+		}
+	}
+
+	public static string Stringify(object val) => JsonSerializer.Serialize(val, opts);
+
+	public static void Stringify(Stream dst, object val) => JsonSerializer.Serialize(dst, val, opts);
+
+	public static T? Parse<T>(string val) => JsonSerializer.Deserialize<T>(val, opts);
+
+	public static T? Parse<T>(Stream val) => JsonSerializer.Deserialize<T>(val, opts);
+}
--- test/IniTest.cs
+++ test/IniTest.cs
@@ -8,14 +8,14 @@ namespace test;
 class Obj {
 	public Color Color { get; set; }
 
-	[Ini(ParserType = typeof(Parsers.ByteSizeParser))] public int SizeA { get; set; }
+	[Ini(ParserType = typeof(Parsers.ByteSizeParser))]
+	public int SizeA { get; set; }
 }
 
 public class IniTest {
 	[Test]
 	public void TestLoad() {
 		var obj = IniLoader.Parse<Obj>($"{EchoPrimaryProject.ProjectRoot()}/test/v.ini");
-		var opt = new JsonSerializerOptions();
-		Console.WriteLine(JsonSerializer.Serialize(obj, options: opt));
+		Console.WriteLine(JSON.Stringify(obj));
 	}
 }
